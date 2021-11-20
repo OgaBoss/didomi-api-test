44912f646f597a4c8cfe172993c3b24ef8691980
ogaboss <goshensoftinc@gmail.com>
2021-11-20 17:14:28 +0100

    - e2e test updates

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -9,7 +9,7 @@ import * as request from 'supertest';
 import { CreateUserDto } from '../src/users/dtos/CreateUserDto';
 import { UsersModule } from '../src/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 import { User } from '../src/users/models/User';
 import { useContainer } from 'class-validator';
 import { Event } from '../src/events/models/Event';
@@ -31,10 +31,8 @@ describe('[API Users] /users', () => {
           password: null,
           database: 'didomi_test',
           entities: [User, Event],
-          synchronize: false,
-          migrations: ['src/migrations/*.js'],
+          synchronize: true,
           dropSchema: true,
-          migrationsRun: true,
         }),
         TypeOrmModule.forFeature([User]),
       ],
@@ -64,7 +62,42 @@ describe('[API Users] /users', () => {
       .send(createUserDto)
       .expect(HttpStatus.CREATED)
       .then(({ body }) => {
-        console.log(body);
+        expect(body.email).toBe('test@test.com');
+        expect(body.consents.length).toBe(0);
+      });
+  });
+
+  it('should get user [GET /users/:id]', async () => {
+    const user = await getRepository(User).find();
+
+    return request(httpServer)
+      .get(`/users/${user[0].id}`)
+      .expect(HttpStatus.OK)
+      .then(({ body }) => {
+        expect(body.email).toBe('test@test.com');
+        expect(body.consents.length).toBe(0);
+      });
+  });
+
+  it('should get all users [GET /users]', async () => {
+    return request(httpServer)
+      .get(`/users`)
+      .expect(HttpStatus.OK)
+      .then(({ body }) => {
+        expect(Array.isArray(body)).toBeTruthy();
+      });
+  });
+
+  it('should delete a user [DELETE /users/:id]', async () => {
+    const user = await getRepository(User).find();
+
+    return request(httpServer)
+      .delete(`/users/${user[0].id}`)
+      .expect(HttpStatus.OK)
+      .then(({ body }) => {
+        expect(body.message).toBe(
+          `User with id #${user[0].id} successfully removed`,
+        );
       });
   });
 
